d5b98dff859f2466dc1ea99553272acd0d87c324
EslamAlaaZaki <32876586+EslamAlaaZaki@users.noreply.github.com>
2022-01-10 13:43:13 +0300

    Add files via upload

--- leet code/67. Add Binary.cpp
+++ leet code/67. Add Binary.cpp
@@ -1,5 +1,7 @@
-#include <iostream>
+
 // https://leetcode.com/problems/add-binary/
+#include <iostream>
+
 using namespace std;
 
 char sum(char a, char b, char& carr)
@@ -25,30 +27,41 @@ char sum(char a, char b, char& carr)
 string Fun(string a , string b)
 {
 	char carr = '0';
-	string output="";
-	int size = (a.length() > b.length()) ? a.length() : b.length();
+	string * output;
+	int size;
+	if (a.length() > b.length()) 
+	{
+		size = a.length();
+		output = &a;
+	}
+	else 
+	{ 
+		size = b.length();
+		output = &b;
+	}
 	for (int i=0 ;i<size;i++)
 	{
 		if ((i < a.length()) && (i < b.length()))
 		{
-			output = sum(a[a.length()-i-1] , b[b.length() - i - 1] , carr)+ output;
+			(*output)[size-i-1] = sum(a[a.length()-i-1] , b[b.length() - i - 1] , carr);
 		}
 		else if((i < a.length()))
 		{
-			output = sum(a[a.length() - i - 1], '0', carr) + output;
+			(*output)[size - i - 1] = sum(a[a.length() - i - 1], '0', carr) ;
 		}
 		else
 		{
-			output = sum('0', b[b.length() - i - 1], carr) + output;
+			(*output)[size - i - 1] = sum('0', b[b.length() - i - 1], carr) ;
 		}
 
 	}
-	if (carr == '1') { return '1' + output; }
-	return output;
+	if (carr == '1') { return '1' + *output; }
+	return *output;
 }
 
 int main()
 {
-	cout << Fun("00001","11110");
+	
+	cout << Fun("11","1");
 
 }
